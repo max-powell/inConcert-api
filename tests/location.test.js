6f1d2568f70aae6fb5496c8d276ba54086dd54b0
max-powell <github email address>
2019-09-09 12:06:03 +0100

    added locations tests

---
+++ tests/location.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest')
+const app = require('../app')
+
+test('Should return a list of locations given a valid search', async () => {
+  const res = await request(app)
+    .get('/locations?query=London')
+    .send()
+    .expect(200)
+
+  const results = res.body
+  expect(results).toEqual(expect.any(Array))
+
+  const londonUK = results[0]
+  expect(londonUK).toEqual({location: 'London, UK', id: expect.any(Number)})
+
+  const londonKY = results[1]
+  expect(londonKY).toEqual({location: 'London, KY, US'})
+})
+
+test('Should return an error given an invalid search', async () => {
+  const res = await request(app)
+    .get('/locations?query=1234')
+    .send()
+    .expect(404)
+
+  const error = res.body
+  expect(error.message).toBeTruthy()
+})
