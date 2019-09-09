1246ecf2d5fa05c4bb33192885fa41ff8a6f964b
max-powell <github email address>
2019-09-09 16:18:48 +0100

    tweaked test to look for presence of object properties. added bad request tests

--- tests/gig.test.js
+++ tests/gig.test.js
@@ -10,6 +10,18 @@ test('Should return a list of gigs given a city and date', async () => {
   const results = res.body
   expect(results).toEqual(expect.any(Array))
 
-  expect(results[0].id).toBe(expect.any(Number))
-  expect(results[0].performance).toBe(expect.any(Object))
+  expect(results[0].id).toBeTruthy()
+  expect(results[0].performance).toBeTruthy()
+})
+
+test('Should return an error if no city or date given', async () => {
+  await request(app)
+    .get('/gigs?date=2019-09-15')
+    .send()
+    .expect(400)
+
+  await request(app)
+    .get('/gigs?location=24426')
+    .send()
+    .expect(400)
 })
