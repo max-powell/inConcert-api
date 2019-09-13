9ea3783543e74e6a6d4f732431c321eaea099ff6
max-powell <github email address>
2019-09-13 11:13:37 +0100

    permitted requests from client origin

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 const locationRouter = require('./routes/location')
 const gigRouter = require('./routes/gig')
 const spotifyRouter = require('./routes/spotify')
+
+const { CLIENT_ROOT_URL } = require('./constants')
 
 const app = express()
 
@@ -14,6 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors({origin: CLIENT_ROOT_URL}))
 
 app.use(locationRouter)
 app.use(gigRouter)
