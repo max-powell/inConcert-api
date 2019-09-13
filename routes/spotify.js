28876a36f22a4c1e6c91163927ac273380095ec8
max-powell <github email address>
2019-09-13 10:25:33 +0100

    changed spotify /callback route to /auth route that responds with object containing tokens/error

--- routes/spotify.js
+++ routes/spotify.js
@@ -2,14 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const SpotifyWebApi = require('spotify-web-api-node')
-const querystring = require('querystring')
 
 const { CLIENT_ROOT_URL } = require('../constants')
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: 'http://localhost:3000/callback'
+  redirectUri: `${CLIENT_ROOT_URL}/auth`
 })
 
 const stateKey = 'spotify_auth_state'
@@ -33,22 +32,13 @@ router.get('/authorize', async (req, res) => {
   res.redirect(authorizeUrl)
 })
 
-router.get('/callback', async (req, res) => {
-  const code = req.query.code
-  const error = req.query.error
-  const state = req.query.state
+router.post('/auth', async (req, res) => {
+  const code = req.body.code
+  const state = req.body.state
   const setState = req.cookies ? req.cookies[stateKey] : null
 
-  if (error) {
-    return res.redirect(`${CLIENT_ROOT_URL}/?` + querystring.stringify({
-      error
-    }))
-  }
-
   if (state === null || state !== setState) {
-    return res.redirect(`${CLIENT_ROOT_URL}/?` + querystring.stringify({
-      error: 'state_mismatch'
-    }))
+    return res.status(401).send({error: 'state_mismatch'})
   }
 
   res.clearCookie(stateKey)
@@ -56,10 +46,10 @@ router.get('/callback', async (req, res) => {
   try {
     const authRes = await spotifyApi.authorizationCodeGrant(code)
 
-    res.redirect(`${CLIENT_ROOT_URL}/auth?` + querystring.stringify({
+    res.send({
       access_token: authRes.body.access_token,
       refresh_token: authRes.body.refresh_token
-    }))
+    })
   } catch (e) {
     console.log(e);
     res.status(500).send(e)
